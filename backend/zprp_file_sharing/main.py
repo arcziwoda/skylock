8c49b5392c046bfc8fd523df1d832956aaf08d8f
akempins <01178535@pw.edu.pl>
2024-10-20 14:12:41 +0200

    docstrings on main

--- backend/zprp_file_sharing/main.py
+++ backend/zprp_file_sharing/main.py
@@ -1,4 +1,16 @@
+"""
+Module Name: hello_world
+
+This module contains a simple program that prints "Hello World!" to the console.
+"""
+
+
 def main():
+    """
+    Main function that prints "Hello World!" to the console.
+
+    This function serves as the entry point for the program when it is executed directly.
+    """
     print("Hello World!")
 
 
