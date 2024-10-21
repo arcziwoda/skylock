b7093f3b07d1c44875872ab4b86a245d9bfab082
Oliwier Szypczyn <01178608@pw.edu.pl>
2024-10-21 20:09:58 +0200

    Very simple commit to reabse with master

--- cli/skylock_cli/app.py
+++ cli/skylock_cli/app.py
@@ -1,83 +1,36 @@
 """
-This module contains commands the user can run to interact with the SkyLock 
+This module contains commands the user can run to interact with the SkyLock
 """
 
-from typing import Optional
-import random
 import typer
-from rich.table import Table
-from rich.console import Console
+from skylock_cli.commands.auth.register import register_user
 
 app = typer.Typer()
-console = Console()
-
-
-def get_name() -> str:
-    """
-    Get a random name.
-
-    Returns:
-        str: A randomly chosen name from a predefined list.
-    """
-    return random.choice(["Deadpool", "Rick", "Morty", "Hiro"])
 
 
 @app.command()
-def hello(
-    name: Optional[str] = typer.Argument(None, help="Name of the person to greet"),
-    uppercase: bool = typer.Option(False, help="Convert the name to uppercase"),
-) -> None:
+def login():
     """
-    Say hello to NAME.
-
-    If NAME is not provided, a random name will be used. If the name is 'root', an error will be raised.
-
-    Args:
-        name (Optional[str]): Name of the person to greet. Defaults to None.
-        uppercase (bool): Convert the name to uppercase. Defaults to False.
-
-    Raises:
-        typer.Abort: If the name is 'root'.
+    Login to the SkyLock
     """
-    if not name:
-        name = get_name()
-    if name == "root":
-        typer.secho("Cannot use 'root' as name!", fg=typer.colors.RED)
-        raise typer.Abort()
-    if uppercase:
-        name = name.upper()
-    print(f"Hello {name}!")
+    typer.echo("Login to the SkyLock")
 
 
 @app.command()
-def goodbye(name: str) -> None:
+def logout():
     """
-    Say goodbye to NAME.
-
-    Args:
-        name (str): Name of the person to say goodbye to.
+    Logout of the SkyLock
     """
-    print(f"Goodbye {name}!")
+    typer.echo("Logout of the SkyLock")
 
 
 @app.command()
-def show_table() -> None:
+def register(user_login: str, user_password: str):
     """
-    Show a sample table using Rich.
-
-    This command demonstrates how to create a simple table using Rich.
+    Register a new user
     """
-    table = Table(title="Sample Table")
-
-    table.add_column("Name", justify="right", style="cyan", no_wrap=True)
-    table.add_column("Age", style="magenta")
-    table.add_column("City", style="green")
-
-    table.add_row("John Doe", "30", "New York")
-    table.add_row("Jane Smith", "25", "Los Angeles")
-    table.add_row("Sam Brown", "22", "Chicago")
-
-    console.print(table)
+    typer.echo(f"Registering new user with login: {user_login}")
+    register_user(user_login, user_password)
 
 
 if __name__ == "__main__":
---
+++ cli/skylock_cli/commands/auth/register.py
@@ -0,0 +1,5 @@
+def register_user(login: str, password: str):
+    """
+    Logic for registering a new user
+    """
+    print("TEST")
