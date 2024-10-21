3472836f17170d8781974624d8bb80af5b5a4b82
akempins <01178535@pw.edu.pl>
2024-10-21 18:33:10 +0200

    Added Dockerfile and docker compose file

--- backend/zprp_file_sharing/main.py
+++ backend/zprp_file_sharing/main.py
@@ -19,4 +19,4 @@ app = FastAPI(title="File Sharing API", version="1.0.0", lifespan=lifespan)
 app.include_router(auth_router, prefix="/api/v1")
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0")
