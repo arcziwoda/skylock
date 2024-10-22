f58f56d11e2dbf1bb64e733b2b32f6af6da88aaf
akempins <01178535@pw.edu.pl>
2024-10-22 12:17:08 +0200

    Refactored get_current_user to return User class instance

--- backend/skylock/core/security.py
+++ backend/skylock/core/security.py
@@ -4,7 +4,10 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
-from .env import JWT_SECRET
+from skylock.core.dependencies import get_user_service
+from skylock.core.env import JWT_SECRET
+from skylock.service.user_service import UserService
+from skylock.service.model.user import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
@@ -12,13 +15,16 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)) -> str:
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    user_service: UserService = Depends(get_user_service),
+) -> User:
     try:
         payload = jwt.decode(token, JWT_SECRET, algorithms=[ALGORITHM])
         username = payload.get("sub")
         if username is None:
             raise HTTPException(status_code=401, detail="Invalid token")
-        return username
+        return user_service.get_user_by_username(username)
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
--- backend/skylock/service/user_service.py
+++ backend/skylock/service/user_service.py
@@ -12,12 +12,6 @@ class UserService:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
 
-    def _hash_password(self, password: str) -> str:
-        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-
-    def _verify_password(self, password: str, hashed_password: str):
-        return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
-
     def register_user(self, username: str, password: str) -> User:
         existing_user = self.user_repository.get_user_by_username(username)
         if existing_user:
@@ -36,3 +30,13 @@ class UserService:
             token = create_jwt_for_user(username)
             return Token(access_token=token, token_type="bearer")
         raise InvalidCredentialsException
+
+    def get_user_by_username(self, username: str) -> User:
+        user_entity = self.user_repository.get_user_by_username(username)
+        return User(id=user_entity.id, username=user_entity.username)
+
+    def _hash_password(self, password: str) -> str:
+        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+    def _verify_password(self, password: str, hashed_password: str):
+        return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
