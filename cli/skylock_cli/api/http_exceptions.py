5b30a0fd128cb7c254cab68da4f68a4bfa8aa4a3
Oliwier Szypczyn <01178608@pw.edu.pl>
2024-10-22 13:52:18 +0200

    Better error handling for register / login

---
+++ cli/skylock_cli/api/http_exceptions.py
@@ -0,0 +1,19 @@
+class SkyLockAPIError(Exception):
+    """Base exception for SkyLock API errors"""
+
+    def __init__(self, message, status_code=None, detail=None):
+        super().__init__(message)
+        self.status_code = status_code
+        self.detail = detail
+
+
+class UserAlreadyExistsError(SkyLockAPIError):
+    """Exception raised when trying to register a user that already exists"""
+
+
+class AuthenticationError(SkyLockAPIError):
+    """Exception raised for authentication errors"""
+
+
+class TokenNotFoundError(SkyLockAPIError):
+    """Exception raised when the token is not found in the response"""
