2db697e83b6220ff5862296089db1de7c4f46dce
Oliwier Szypczyn <01178608@pw.edu.pl>
2024-10-30 10:01:33 +0100

    Started implementing rmdir and added make script to see the coverage of tests in my project

--- cli/skylock_cli/api/dir_requests.py
+++ cli/skylock_cli/api/dir_requests.py
@@ -34,3 +34,30 @@ def send_mkdir_request(token: Token, path: Path) -> None:
 
     if response.status_code != HTTPStatus.CREATED:
         raise api_exceptions.SkyLockAPIError("Failed to create directory (Internal Server Error)")
+
+
+def send_rmdir_request(token: Token, path: Path) -> None:
+    """
+    Send a rmdir request to the SkyLock backend API.
+
+    Args:
+        token (Token): The token object containing authentication token.
+        path (str): The path of the directory to be deleted.
+    """
+    url = "/folders" + quote(str(path))
+    auth = bearer_auth.BearerAuth(token)
+
+    response = client.delete(
+        url=url,
+        auth=auth,
+        headers=API_HEADERS,
+    )
+
+    if response.status_code == HTTPStatus.UNAUTHORIZED:
+        raise api_exceptions.UserUnauthorizedError()
+
+    if response.status_code == HTTPStatus.NOT_FOUND:
+        raise api_exceptions.DirectoryNotFoundError(path)
+
+    if response.status_code != HTTPStatus.NO_CONTENT:
+        raise api_exceptions.SkyLockAPIError("Failed to delete directory (Internal Server Error)")
