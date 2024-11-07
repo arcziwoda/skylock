0e94178c55782aebd0763a71bec59e7dab9ce23f
Oliwier Szypczyn <01178608@pw.edu.pl>
2024-11-07 22:33:14 +0100

    Refactor file upload logic to use a standard for FastAPI

--- cli/tests/core/test_file_operations.py
+++ cli/tests/core/test_file_operations.py
@@ -25,7 +25,9 @@ class TestGenerateUniqueFilePath(unittest.TestCase):
         result = _generate_unique_file_path(directory, file_name)
 
         self.assertEqual(result, Path("/mocked/path/file.txt"))
-        mock_tempfile.assert_called_once_with(dir=directory, prefix="file", suffix=".txt", delete=False)
+        mock_tempfile.assert_called_once_with(
+            dir=directory, prefix="file", suffix=".txt", delete=False
+        )
 
     @patch("skylock_cli.core.file_operations.tempfile.NamedTemporaryFile")
     def test_generate_unique_file_path_with_different_extension(self, mock_tempfile):
@@ -39,7 +41,9 @@ class TestGenerateUniqueFilePath(unittest.TestCase):
         result = _generate_unique_file_path(directory, file_name)
 
         self.assertEqual(result, Path("/mocked/path/file.pdf"))
-        mock_tempfile.assert_called_once_with(dir=directory, prefix="file", suffix=".pdf", delete=False)
+        mock_tempfile.assert_called_once_with(
+            dir=directory, prefix="file", suffix=".pdf", delete=False
+        )
 
     @patch("skylock_cli.core.file_operations.tempfile.NamedTemporaryFile")
     def test_generate_unique_file_path_with_no_extension(self, mock_tempfile):
@@ -53,7 +57,9 @@ class TestGenerateUniqueFilePath(unittest.TestCase):
         result = _generate_unique_file_path(directory, file_name)
 
         self.assertEqual(result, Path("/mocked/path/file"))
-        mock_tempfile.assert_called_once_with(dir=directory, prefix="file", suffix="", delete=False)
+        mock_tempfile.assert_called_once_with(
+            dir=directory, prefix="file", suffix="", delete=False
+        )
 
 
 if __name__ == "__main__":
