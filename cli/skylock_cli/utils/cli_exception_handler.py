d5bec5adb63b27dc99bcf1a325acd178d24489d2
Oliwier Szypczyn <oli.szypczyn@gmail.com>
2024-11-15 14:24:45 +0100

    fix: update error messages for connection errors in CLI exception handling

--- cli/skylock_cli/utils/cli_exception_handler.py
+++ cli/skylock_cli/utils/cli_exception_handler.py
@@ -5,6 +5,7 @@ Module to handle API exceptions and display them to the user using the typer lib
 import typer
 from rich.console import Console
 from rich.traceback import Traceback
+from httpx import ConnectError
 from skylock_cli.exceptions import api_exceptions, core_exceptions
 
 err_console = Console(stderr=True)
@@ -33,8 +34,15 @@ class CLIExceptionHandler:
         ):
             err_console.print(f"[red]{exc_value}[/red]")
             raise typer.Exit(code=1)
+        if isinstance(exc_value, ConnectError):
+            err_console.print(
+                "[red]The server is not reachable at the moment. Please try again later.[/red]"
+            )
+            raise typer.Exit(code=1)
         if exc_type is not None:
-            tb = Traceback.from_exception(exc_type, exc_value, exc_traceback, max_frames=5)
+            tb = Traceback.from_exception(
+                exc_type, exc_value, exc_traceback, max_frames=5
+            )
             err_console.print(tb)
             raise typer.Exit(code=1)
 
