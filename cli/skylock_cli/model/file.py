7451663e0ff35ec724270f5b00318631a39d48c4
Oliwier Szypczyn <oli.szypczyn@gmail.com>
2024-11-21 20:30:19 +0100

    refactor(models): change Directory and File classes to inherit from Resource and add type_label field

--- cli/skylock_cli/model/file.py
+++ cli/skylock_cli/model/file.py
@@ -4,11 +4,12 @@ Module with File model class
 
 from pathlib import Path
 from typing import Annotated, Optional
-from pydantic import BaseModel, Field
+from pydantic import Field
 from typer.colors import YELLOW
+from skylock_cli.model.resource import Resource
 
 
-class File(BaseModel):
+class File(Resource):
     """Stores file metadata"""
 
     name: Annotated[str, Field(description="File name")]
@@ -16,3 +17,4 @@ class File(BaseModel):
     color: Annotated[
         Optional[str], Field(description="File color used to pretty print in CLI")
     ] = YELLOW
+    type_label: Annotated[str, Field(description="Type of resource")] = "file"
